6bc9ecdcf78bdc1206bb92125fb4d8fdae71bd3b
Louis Le Doussal <louis.le-doussal@mmibordeaux.com>
2026-02-22 11:23:54 +0100

    test pre commit

---
+++ eslint.config.mjs
@@ -0,0 +1,27 @@
+import antfu from "@antfu/eslint-config";
+// @ts-check
+import withNuxt from "./.nuxt/eslint.config.mjs";
+
+export default withNuxt(antfu({
+  type: "app",
+  typescript: true,
+  formatters: true,
+  stylistic: {
+    indent: 2,
+    semi: true,
+    quotes: "double",
+  },
+}, {
+  rules: {
+    "ts/consistent-type-definitions": ["error", "type"],
+    "no-console": ["warn"],
+    "antfu/no-top-level-await": ["off"],
+    "node/no-process-env": "off",
+    "node/prefer-global/process": "off",
+    "perfectionist/sort-imports": "error",
+    "unicorn/filename-case": ["error", {
+      case: "kebabCase",
+      ignore: ["README.md"],
+    }],
+  },
+}));
