43fd6cb14db2cf32ecc13e4e1bdea04c06b95de3
Louis Le Doussal <louis.le-doussal@mmibordeaux.com>
2026-04-06 00:32:43 +0200

    feat: ajoute une grande partie du portfolio

--- lib/env.ts
+++ lib/env.ts
@@ -2,8 +2,8 @@ import { z } from "zod";
 
 const EnvSchema = z.object({
   NODE_ENV: z.string(),
-  TURSO_DATABASE_URL: z.string(),
-  TURSO_AUTH_TOKEN: z.string(),
+  TURSO_DATABASE_URL: z.string().optional(),
+  TURSO_AUTH_TOKEN: z.string().optional(),
 });
 
 export type EnvSchema = z.infer<typeof EnvSchema>;
